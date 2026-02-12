987a11738987ad9548717cbd53f34422d91f7d34
Amrutha <amruthasirisha88@gmail.com>
2026-02-12 11:55:36 +0530

    actions

---
+++ rabbitmq.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+MYSQL_HOST=mysql.devops1.online
+
+
+if [ $USERID -ne 0 ]; then
+   echo -e "$R please run this script with root user access $N" | tee -a $LOGS_FILE
+   exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+       echo -e "$2...... $R failure $N" | tee -a $LOGS_FILE
+       exit 1
+    else
+       echo -e "$2...... $G success $N" | tee -a $LOGS_FILE
+    fi
+}
+
+cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+VALIDATE $? "Added RabbitMQ.repo"
+
+dnf install rabbitmq-server -y
+VALIDATE $? "Installing RabbitMQ server"
+
+systemctl enable rabbitmq-server
+systemctl start rabbitmq-server
+VALIDATE $? "Enable and started RabbitMQ"
+
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+VALIDATE $? "Created user and given permissions"
